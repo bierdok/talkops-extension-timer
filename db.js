55b24bce991dba453bf9b132efa629658ed731b3
bierdok <2333728+bierdok@users.noreply.github.com>
2025-03-10 23:32:45 +0100

    Reformat code

--- db.js
+++ db.js
@@ -1,16 +1,16 @@
-import { JSONFilePreset } from "lowdb/node";
+import { JSONFilePreset } from 'lowdb/node'
 
-const db = await JSONFilePreset("/data/db.json", { timers: [] });
+const db = await JSONFilePreset('/data/db.json', { timers: [] })
 
 export function addTimer(timer) {
-  db.update(({ timers }) => timers.push(timer));
+  db.update(({ timers }) => timers.push(timer))
 }
 
 export function removeTimer(timer) {
-  db.data.timers = db.data.timers.filter((t) => t !== timer);
-  db.write();
+  db.data.timers = db.data.timers.filter((t) => t !== timer)
+  db.write()
 }
 
 export function getTimers() {
-  return db.data.timers;
+  return db.data.timers
 }
