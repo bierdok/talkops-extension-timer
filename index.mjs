fbfbadba6df40d23c571050a5292b28143c33f5f
bierdok <2333728+bierdok@users.noreply.github.com>
2025-03-10 15:37:28 +0100

    Update

--- index.mjs
+++ index.mjs
@@ -1,10 +1,10 @@
-import { Alarm, Extension, Notification, Readme, Service } from "talkops";
-import prettyMilliseconds from "pretty-ms";
-import yaml from "js-yaml";
+import { Alarm, Extension, Notification, Readme, Service } from 'talkops'
+import prettyMilliseconds from 'pretty-ms'
+import yaml from 'js-yaml'
 
-const extension = new Extension("Timer");
-extension.setDockerRepository("bierdok/talkops-extension-timer");
-extension.enableDockerVolumeData();
+const extension = new Extension('Timer')
+extension.setDockerRepository('bierdok/talkops-extension-timer')
+extension.enableDockerVolumeData()
 
 extension.setDescription(`
 This Extension allows to manage timers **by voice in real-time**.
@@ -13,52 +13,52 @@ This Extension allows to manage timers **by voice in real-time**.
 * Create a timer
 * Check timer states
 * Cancel a timer
-`);
+`)
 
 const baseInstructions = `
 You can manage multiple timers.
 It is possible to add another timer for the same duration.
-`;
+`
 
-import timersModel from "./schemas/models/timers.json" with { type: "json" };
+import timersModel from './schemas/models/timers.json' with { type: 'json' }
 
 extension.setInstructions(() => {
-  const instructions = [baseInstructions];
-  instructions.push("``` yaml");
-  instructions.push(yaml.dump({ timersModel }));
-  instructions.push("```");
-  return instructions;
-});
+  const instructions = [baseInstructions]
+  instructions.push('``` yaml')
+  instructions.push(yaml.dump({ timersModel }))
+  instructions.push('```')
+  return instructions
+})
 
-import create_timer from "./schemas/functions/create_timer.json" with { type: "json" };
-import get_timers from "./schemas/functions/get_timers.json" with { type: "json" };
-import delete_timer from "./schemas/functions/delete_timer.json" with { type: "json" };
+import create_timer from './schemas/functions/create_timer.json' with { type: 'json' }
+import get_timers from './schemas/functions/get_timers.json' with { type: 'json' }
+import delete_timer from './schemas/functions/delete_timer.json' with { type: 'json' }
 
-extension.setFunctionSchemas([create_timer, get_timers, delete_timer]);
+extension.setFunctionSchemas([create_timer, get_timers, delete_timer])
 
-import { addTimer, removeTimer, getTimers } from "./db.js";
+import { addTimer, removeTimer, getTimers } from './db.js'
 
 function getClientTimers(clientId) {
-  return getTimers().filter((timer) => timer.clientId === clientId);
+  return getTimers().filter((timer) => timer.clientId === clientId)
 }
 
 function getNextClientTimerNumber(clientId) {
-  let number = 1;
+  let number = 1
   for (const timer of getClientTimers(clientId)) {
-    if (timer.number < number) continue;
-    number = timer.number + 1;
+    if (timer.number < number) continue
+    number = timer.number + 1
   }
-  return number;
+  return number
 }
 
 extension.setFunctions([
   function create_timer(duration, clientId) {
-    duration = duration * 1000;
-    const number = getNextClientTimerNumber(clientId);
-    const createdAt = new Date().getTime();
-    const completeAt = createdAt + duration;
-    addTimer({ number, createdAt, completeAt, duration, clientId });
-    return `The timer ${number} has been created.`;
+    duration = duration * 1000
+    const number = getNextClientTimerNumber(clientId)
+    const createdAt = new Date().getTime()
+    const completeAt = createdAt + duration
+    addTimer({ number, createdAt, completeAt, duration, clientId })
+    return `The timer ${number} has been created.`
   },
   function get_timers(clientId) {
     return yaml.dump(
@@ -68,41 +68,38 @@ extension.setFunctions([
           duration: prettyMilliseconds(timer.duration, {
             verbose: true,
           }),
-          timeleft: prettyMilliseconds(
-            timer.completeAt - new Date().getTime(),
-            {
-              verbose: true,
-              separateMilliseconds: true,
-            }
-          ),
-        };
-      })
-    );
+          timeleft: prettyMilliseconds(timer.completeAt - new Date().getTime(), {
+            verbose: true,
+            separateMilliseconds: true,
+          }),
+        }
+      }),
+    )
   },
   function delete_timer(number, clientId) {
     for (const timer of getClientTimers(clientId)) {
-      if (timer.number !== number) continue;
-      removeTimer(timer);
-      return `The timer ${number} has been deleted.`;
+      if (timer.number !== number) continue
+      removeTimer(timer)
+      return `The timer ${number} has been deleted.`
     }
-    return "Not found.";
+    return 'Not found.'
   },
-]);
+])
 
 setInterval(() => {
-  const now = new Date().getTime();
+  const now = new Date().getTime()
   for (const timer of getTimers()) {
-    if (timer.completeAt > now) continue;
-    removeTimer(timer);
+    if (timer.completeAt > now) continue
+    removeTimer(timer)
     service.send([
       new Alarm().setFrom(extension.name).addTo(timer.clientId),
       new Notification()
         .setFrom(extension.name)
         .addTo(timer.clientId)
         .setText(`The timer ${timer.number} is complete.`),
-    ]);
+    ])
   }
-}, 1000);
+}, 1000)
 
-new Readme(process.env.README_TEMPLATE_URL, "/app/README.md", extension);
-const service = new Service(process.env.AGENT_URLS.split(","), extension);
+new Readme(process.env.README_TEMPLATE_URL, '/app/README.md', extension)
+const service = new Service(process.env.AGENT_URLS.split(','), extension)
